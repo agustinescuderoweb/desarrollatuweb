98503f65d13499f93b0438e985047c0f46eee916
Agustin <agustinescuderoweb@gmail.com>
2024-12-12 20:40:04 -0300

    nuevos

--- src/componentes/landing.tsx
+++ src/componentes/landing.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Link from 'next/link';
 import styles from "@/componentes/landing.module.css";
-import x from "../../public/icon/x-circle-fill.svg";
 import check from "../../public/icon/check-circle-fill.svg";
 import como from "../../public/como.png";
 import Image from 'next/image';
@@ -10,7 +9,7 @@ function landing() {
   return (
     <div className={styles.contenido} id="seccion">
       <div className={styles.guia}>
-        <h1>Descarga la guía Gratis</h1>
+        <h1>Descarga la guía</h1>
         <h2>¿Cómo impulsar tu presencia en linea con una landing Page?</h2>
         <Image className={styles.imagen} src={como} width={400} alt="imag"></Image>
         <button className={styles.button}><Link href={"https://drive.google.com/file/d/1TiDH8ev3BzUUw1w-GCxVO2165fxM4CCV/view?usp=drive_link"}>Descargar la Guía!</Link></button>
