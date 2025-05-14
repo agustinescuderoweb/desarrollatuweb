f7afb8267b44359b1a466f64914955adc9143e31
Agustín Escudero Web <124199392+agustinescuderoweb@users.noreply.github.com>
2025-05-14 07:48:03 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Head from "next/head";
+import Image from 'next/image';
 
 export const metadata: Metadata = {
   title: "Desarrolla tu Sitio Web desde cero",
@@ -37,7 +38,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             }}
           />
           <noscript>
-            <img
+            <Image
               height="1"
               width="1"
               style={{ display: 'none' }}
